61c1900290fa4065bf1bb1617ea7b6f3121976af
Mirokshi <43352881+mirokshi@users.noreply.github.com>
2022-11-10 13:31:07 +0100

    --

--- src/index.js
+++ src/index.js
@@ -4,28 +4,31 @@ const path = require("path");
 const { createPublicKey } = require("crypto");
 const sql = require('mssql');
 
-if(process.env.NODE_ENV !== 'production') {
-  require('electron-reload')(__dirname,{
-    electron: path.join(__dirname,'../node_modules','.bin','electron')
-  })
-}
+/*Comentar al pasar a PROD*/
+// if(process.env.NODE_ENV !== 'production') {
+//   require('electron-reload')(__dirname,{
+//     electron: path.join(__dirname,'../node_modules','.bin','electron')
+//   })
+// }
 
 app.on('ready',() => {
   let mainWindow = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
-    }
+    },
+    frame:false
   });
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname,'views/index.html'),
     protocol: 'file',
     slashes: true
   }))
-
-  // mainWindow.setMenu(null);
-  const mainMenu = Menu.buildFromTemplate(templateMenu);
-  Menu.setApplicationMenu(mainMenu)
+  /*Descomentar al pasar a prod*/
+  mainWindow.setMenu(null);
+  /*Comentar al pasar a prod*/
+  // const mainMenu = Menu.buildFromTemplate(templateMenu);
+  // Menu.setApplicationMenu(mainMenu)
 });
 
 const templateMenu = [
